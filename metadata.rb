765697fe2b3781df7b0a801fef4359f6c9ea455f
sura8257 <thapa.suraj751@gmail.com>
2015-06-23 20:24:32 +1000

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'thapa.suraj751@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures Chef-ProjectHaibung'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.16.3'
+version '0.16.8'
 
 depends 'yum-epel'
 depends 'php'
